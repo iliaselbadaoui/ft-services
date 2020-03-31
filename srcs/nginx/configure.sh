cc4b7d720fbb59411769d148ff4cbbf8c30e2b05
Ilias Elbadaoui <iliassovich@outlook.com>
2020-03-31 18:27:53 +0100

    do not touche images again

--- srcs/nginx/configure.sh
+++ srcs/nginx/configure.sh
@@ -7,6 +7,10 @@ mv /nginx.conf /etc/nginx/nginx.conf
 mv index.html /www/index.html
 mkdir /run/openrc/
 touch /run/openrc/softlevel
-
+adduser -D square
+echo square:aitfaska98 | chpasswd
+chmod -R go-r /etc/ssh/ssh_host_rsa_key
 rc-status
 rc-service nginx start
+rc-service sshd start
+tail -F /dev/null
